cc4a1ab38f8a902598c4c9998b0bd86abf9ac358
titli1981 <madhumishra084@gmailcom>
2016-09-07 06:38:52 -0400

    Added Jason response

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,19 @@
 class BooksController < ApplicationController
 	def index
 		@books = Book.all
+
 		respond_to do |format|
 			format.html
-			format.text
+			format.text 
+
 			format.csv do
 				render plain: Book.generate_csv(@books)
 			end
-			format.json
+
+			format.json do
+				render json: @books
+			end
+
 		end
 	end
 
